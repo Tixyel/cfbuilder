513984fe1152c5dff0da689507c39a4ea3e11a33
Cauã Diego <tixyelpvd@gmail.com>
2024-03-29 02:52:23 -0300

    Added information to dropdown fields

--- src/components/inputs/dropdown.jsx
+++ src/components/inputs/dropdown.jsx
@@ -2,11 +2,15 @@ import { Plus } from 'lucide-react'
 
 export default function DropdownInput({}) {
   return (
-    <div className="w-full max-w-sm flex flex-row justify-end items-center gap-1.5">
-      <p className="text-zinc-50 text-sm ml-2">Add option</p>
-      <div className="grid place-items-center size-10  cursor-pointer">
-        <Plus color="#ffffff" size="20px" />
+    <div className="flex w-full flex-col justify-end items-end">
+      <div className="w-full max-w-sm flex flex-row justify-end items-center gap-1.5">
+        <p className="text-zinc-50 text-sm ml-2">Add option</p>
+        <div className="grid place-items-center size-10  cursor-pointer">
+          <Plus color="#ffffff" size="20px" />
+        </div>
       </div>
+
+      <p className="text-zinc-400 text-sm   px-6">Not yet implemented</p>
     </div>
   )
 }
