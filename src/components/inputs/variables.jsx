38818019fc0630023d12c873a85b0611bed77ee4
Cauã Diego <tixyelpvd@gmail.com>
2024-03-29 15:52:58 -0300

    checkbox

--- src/components/inputs/variables.jsx
+++ src/components/inputs/variables.jsx
@@ -3,12 +3,15 @@ import ColorpickerInput from '@/components/inputs/colorpicker'
 import GoogleFontInput from '@/components/inputs/googleFont'
 import TextInput from '@/components/inputs/text'
 import { Input } from '../ui/input'
+import { Checkbox } from '../ui/checkbox'
 
 export default function Variable({ type, index = '', onChange, value }) {
   const variables = {
     'googleFont': <GoogleFontInput value={value} index={index} id="value" onChange={onChange} />,
     'colorpicker': <ColorpickerInput value={value} index={index} onChange={onChange} />,
-    'checkbox': <Input type="checkbox" value={value} index={index} oonChange={onChange} />,
+    'checkbox': (
+      <Checkbox className="w-full h-8" value={value} index={index} onCheckedChange={(value) => onChange({ target: { value, id: 'value' } }, index)} />
+    ),
   }
 
   return (
